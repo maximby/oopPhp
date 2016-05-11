507d43c3365a56fedbb1a26e88bbc5ee24039e6a
Maxim Zzzz <z_connect@mail.ru>
2016-05-11 20:25:02 +0300

    Добавлены магические методы set и get

--- class.Address.inc.php
+++ class.Address.inc.php
@@ -14,10 +14,71 @@ class Address {
     public $subdivision_name;
 
     // Postal code.
-    public $postal_code;
+    protected $_postal_code;
 
     // Name of the Country.
     public $country_name;
+
+    // Primary key of an Address.
+    protected $_address_id;
+
+    // When the record was created and last updated.
+    protected $_time_created;
+    protected $_time_updated;
+
+    /**
+     * Magic _get.
+     * @param string $name
+     * @return mixed
+     */
+    function __get($name) {
+        // Postal code lookup if unset.(Просмотр индекса если он не задан)
+        if (!$this->_postal_code) {
+            $this->_postal_code = $this->_postal_code_guess();
+        }
+
+        // Attempt to return a protected property by name.
+        // Попытка вернуть защищенное свойство по имени
+        $protected_property_name = '_' . $name;
+        if (property_exists($this, $protected_property_name)) {
+            return $this->$protected_property_name;
+        }
+
+        // Unable to access property: trigger error.
+        // Не возможно обратиться к свойству; trigger error
+        trigger_error('Undefined property via __get: ', $name);
+        return null;
+    }
+
+    /**
+     * Magic __set.
+     * @param string $name
+     * @param mixed $value
+     */
+    function __set($name,$value) {
+        // Allow anything to set the postal code.
+        // Разрешить всем задавать почтовый индекс
+        if ('postal_code' == $name) {
+            $this->$name = $value;
+            return;
+        }
+
+        // Unable to access property: trigger error.
+        // Не возможно обратиться к свойству; trigger error
+        trigger_error('Undefined or unallowed property via __set(): . $name');
+            //Неопределенное или недопустимое свойство метода __set
+
+
+    }
+
+    /**
+     * Guess the postal code given the subdivision and city name.
+     * @todo Replace with a database lookup.
+     * @return string
+     */
+    protected function _postal_code_guess() {
+        return 'LOOKUP';
+    }
 
     /**
      * Display an address in HTML.
@@ -36,6 +97,7 @@ class Address {
         // City, Subdivision Postal.
         $output .= '<br/>';
         $output .= $this->city_name . ', ' . $this->subdivision_name;
+        $output .= ' ' . $this->postal_code;
 
         // Country.
         $output .= '<br/>';
--- demo.php
+++ demo.php
@@ -13,8 +13,13 @@ $address->street_address_1 = '555 Fake Street';
 $address->city_name = 'Townsville';
 $address->subdivision_name = 'State';
 $address->postal_code = '12345';
+$address->postal_code = '12345';
 $address->country_name = 'United States of America';
 echo '<tt><pre>' . var_export($address,true) . '</pre></tt>';
 
 echo '<h2>Displaying address...</h2>';
 echo $address->display();
+
+echo '<h2>Testing magic __get and __set.</h2>';
+unset($address->postal_code);
+echo$address->display();
