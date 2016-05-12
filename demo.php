f8b54f43c1350d50a747b981fc72b2bf66568a78
Maxim Zzzz <z_connect@mail.ru>
2016-05-12 14:33:05 +0300

    Реализовали метод postal_code_guess()

--- demo.php
+++ demo.php
@@ -1,25 +1,20 @@
 <?php
 
 require 'class.Address.inc.php';
+require 'class.Database.inc.php';
 
 echo '<h2>Instantiating Address</h2>';
 $address = new Address();
 
-echo '<h2>Empty Address</h2>';
-echo '<tt><pre>' . var_export($address,true) . '</pre></tt>';
+
 
 echo '<h2>Setting properties...</h2>';
 $address->street_address_1 = '555 Fake Street';
 $address->city_name = 'Townsville';
 $address->subdivision_name = 'State';
-$address->postal_code = '12345';
 $address->country_name = 'United States of America';
 $address->address_type_id = 1;
-echo '<tt><pre>' . var_export($address,true) . '</pre></tt>';
-
-echo '<h2>Displaying address...</h2>';
-echo $address->display();
-
+echo $address;
 echo '<h2>Testing magic __get and __set.</h2>';
 unset($address->postal_code);
 echo$address->display();
@@ -30,22 +25,9 @@ $address_2 = new Address(array(
     'street_address_1' => '123 Phone Ave',
     'city_name' => 'Villageland',
     'subdivision_name' => 'Region',
-    'postal_code' => '67890',
     'country_name' => 'Canada',
 ));
 echo $address_2->display();
 
 echo '<h2>Address __toString</h2>';
 echo $address_2;
-
-echo '<h2>Displaying address types...</h2>';
-echo '<tt><pre>' . var_export(Address::$valid_address_types, true) . '</pre></tt>';
-
-// Тестирование проверки индентификатора типа Address
-echo '<h2>Testing address type ID validation</h2>';
-for ($id = 0; $id <= 4; $id++) {
-    echo "<div>$id: ";
-    echo Address::isValidAddressTypeId($id) ? 'Valid' : 'Invalid';
-    echo "</div>";
-}
-
